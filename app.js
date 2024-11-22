96675237b7a18b33845bf8f134203b838b9b2bab
zhouxy <15292021323@163.com>
2024-11-22 17:17:45 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const userRouter = require('./router/user')
+
+const app = express()
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use('/api', userRouter)
+
+app.listen(3007, () => {
+  console.log('启动服务器 http://127.0.0.1:3007')
+})
