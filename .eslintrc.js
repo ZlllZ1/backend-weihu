e49bdd753c64982715d5674b2dc200c6723a2a46
zhouxy <15292021323@163.com>
2024-11-25 11:19:14 +0800

    feat: 代码格式化修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
 		node: true,
 		es6: true
 	},
-	// extends: ['plugin:vue/essential', '@vue/standard'],
 	extends: ['plugin:vue/recommended', 'eslint:recommended'],
 	parserOptions: {
 		parser: 'babel-eslint',
@@ -18,13 +17,9 @@ module.exports = {
 		'no-irregular-whitespace': 'off',
 		'vue/script-indent': ['error', 4, { baseIndent: 1 }],
 		'space-before-function-paren': [0, 'always'],
-		// 强制在注释中 // 或 /* 使用一致的空格
 		'spaced-comment': 0,
-		// always-multiline：多行模式必须带逗号，单行模式不能带逗号
 		'comma-dangle': [1, 'never'],
-		// 末尾禁止使用分号
 		'no-mixed-spaces-and-tabs': [1, 'smart-tabs'],
-		// semi: 0,
 		'no-tabs': 0,
 		'linebreak-style': ['off', 'windows'],
 		quotes: ['error', 'single'],
