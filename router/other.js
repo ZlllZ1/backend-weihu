8c38147aa97e35df982f2c26f80a036731de79f8
zhouxy <15292021323@163.com>
2024-12-05 10:51:31 +0800

    feat: 忘记密码实现

--- router/other.js
+++ router/other.js
@@ -4,4 +4,6 @@ const router = express.Router()
 
 router.post('/commitErrorLog', otherHandler.commitErrorLog)
 
+router.post('/changePassword', otherHandler.changePassword)
+
 module.exports = router
--- router_handler/other.js
+++ router_handler/other.js
@@ -1,4 +1,6 @@
 const ErrorLog = require('../mongodb/errorLog.js')
+const User = require('../mongodb/user.js')
+const bcrypt = require('bcrypt')
 
 const commitErrorLog = async (req, res) => {
 	const { errorContent } = req.body
@@ -13,6 +15,24 @@ const commitErrorLog = async (req, res) => {
 	}
 }
 
+const changePassword = async (req, res) => {
+	const { account, password } = req.body
+	console.log(account, password)
+	if (!account || !password) return res.sendError(400, 'account or password is required')
+	try {
+		const user = await User.findOne({ email: account })
+		if (!user) return res.sendError(404, 'User not found')
+		const hashedPassword = await bcrypt.hash(password, 10)
+		user.password = hashedPassword
+		await user.save()
+		res.sendSuccess({ message: 'Password changed successfully' })
+	} catch (error) {
+		console.error('Error in changePassword:', error)
+		res.sendError(500, 'Internal server error')
+	}
+}
+
 module.exports = {
-	commitErrorLog
+	commitErrorLog,
+	changePassword
 }
