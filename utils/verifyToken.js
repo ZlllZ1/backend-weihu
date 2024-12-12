3743b929a1c732b8f461c4b2486772caae3f81fb
zhouxy <15292021323@163.com>
2024-12-12 09:16:22 +0800

    feat: token中间件处理,新增email至req

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 
 const verifyToken = (req, res, next) => {
 	const token = req.headers['authorization']?.split(' ')[1]
-	if (!token) return res.sendError(401, 'Unauthorized token')
+	if (!token) return res.sendError(400, 'no token')
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET)
 		req.userId = new mongoose.Types.ObjectId(decoded._id)
+		req.email = decoded.email
 		next()
 	} catch (error) {
 		if (error instanceof jwt.TokenExpiredError) return res.sendError(401, 'Token expired')
